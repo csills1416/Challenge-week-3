0f55a122378d6c889d4a16dca86da0468f1c9912
CSillus <71670415+csills1416@users.noreply.github.com>
2023-05-29 16:22:09 -0400

    earliercommit-removed Readme for !. I didn't think I needed it.

--- Develop/script.js
+++ Develop/script.js
@@ -5,54 +5,41 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); {
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericChars = "0123456789";
+    var specialChars = "!@#$%^&*()_-+=";
+  
+    var includeLowercase = confirm("Include Lowercase Letters?")
+    var includeUppercase = confirm("Include Uppercase Letters?")
+    var includeNumeric = confirm("Include numbers?")
+    var includeSpecial = confirm("Include special characters?")
+  }
   var passwordText = document.querySelector("#password");
+
+  var passwordChars = "";
+    if (includeUppercase) {
+      passwordChars += uppercaseChars;
+    }
+    if (includeLowercase) {
+      passwordChars += lowercaseChars;
+    }
+    if (includeNumeric) {
+      passwordChars += numericChars;
+    }
+    if (includeSpecial) {
+      passwordChars += specialChars;
+    }
+}
+
+var passwordLength = prompt("How long do you want your password to be?");
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Invalid password: Password must be between 8 and 128 characters");
 
   passwordText.value = password;
 
-}
-
-function generatePassword() {
-  var passwordLength = prompt ("How Long do you want your password to be?")
-  if (passwordLength < 8, passwordLength > 128)
-    password = true;
-    else
-    (passwordLength > 8, passwordLength < 128)
-    password = false;
-}
-//https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript#:~:text=To%20generate%20a%20random%20word,random().
-  var password = "";
-  for (var i = 0; i < passwordLength; i++)
-//https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript#:~:text=To%20generate%20a%20random%20word,random().
-
-function writePassword () {
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "0123456789";
-var specialChars = "!@#$%^&*()_-+=";
-
-var includeLowercase = confirm("Include Lowercase Letters?")
-var includeUppercase = confirm("Include Uppercase Letters?")
-var includeNumeric = confirm("Include numbers?")
-var includeSpecial = confirm("Include special characters?")
-}
-
-var passwordChars = "";
-
-if (includeUppercase) {
-  passwordChars += uppercaseChars;
-}
-
-if (includeNumeric) {
-  passwordChars += numericChars;
-}
-
-if (includeSpecial) {
-  passwordChars += specialChars;
-}
-
-if (includeLowercase) {
-  passwordChars += lowercaseChars;
 }
 
 // Add event listener to generate button
