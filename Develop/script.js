fe86100c60e05016451ffa7cfb9b965fa894d85f
CSillus <71670415+csills1416@users.noreply.github.com>
2023-05-30 17:07:10 -0400

    fixed indention issue, fixed password generator, removed excessive code

--- Develop/script.js
+++ Develop/script.js
@@ -32,28 +32,25 @@ if (includeSpecial) {
 
 var passwordLength = prompt("How long do you want your possword to be?");
 
-if (isNAN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
   alert("Invalid password: Password must be between 8 and 128 characters");
 }
 
-var password = "";
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordChars.length);
+    password += passwordChars.charAt(randomIndex);
+  }
+  return password;
+}
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(); {
-
-    for (var i = 0; i < passwordLength; i++) {
-      var randomIndex = Math.floor(Math.random()) * passwordChars.length
-      password += passwordChars. charAt(randomIndex);
-    }    
-
-  }
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-}
-
-  passwordText.value = password;
-
+  }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
